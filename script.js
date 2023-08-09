2ca4950d0d2c1618e528d193d5659e94c3e59212
[Renan Ducasble] <41059946+renanducasble@users.noreply.github.com>
2023-08-09 00:01:17 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// FUNÇÃO PARA LIGAR E DESLIGAR O BOTÃO
+
+function toggleMode() {
+  const html = document.documentElement
+  const avatarFoto = document.querySelector("#profile img")
+  const fraseSemFoto = document.querySelector("#profile img")
+  html.classList.toggle("light")
+
+  /* LEIA A FRASE ABAIXO DA FUNÇÃO
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  
+  }
+  */
+
+  // JÁ EXISTE UMA FUNÇÃO BUILT-IN PARA FAZER TUDO ISSO, CHAMADA TOGGLE
+  if (html.classList.contains("light")) {
+    avatarFoto.setAttribute("src", "./assets/Avatar.pn")
+    fraseSemFoto.setAttribute("alt", "Outra resposta")
+  } else {
+    avatarFoto.setAttribute("src", "./assets/foto_facebook.jpg")
+  }
+}
